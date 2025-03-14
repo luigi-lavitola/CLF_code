d9f0699918b35311ab0dfcf2a78938f84653f617
Gennaro Tortone <gennaro.tortone@na.infn.it>
2025-03-14 11:37:43 +0000

    update CLF_cli.py

--- CLI_tool/CLF_cli_old2.py
+++ CLI_tool/CLF_cli_old2.py
@@ -23,7 +23,6 @@ from lib.RPC import RPCDevice
 from lib.Centurion import Centurion
 from lib.Configuration import Configuration
 from lib.DeviceCollection import DeviceCollection
-from lib.Radiometer import Radiometer3700, RadiometerOphir
 
 cfg = Configuration()
 cfg.read()
@@ -42,7 +41,7 @@ class CLF_app(cmd2.Cmd):
 
         self.param = param
         self.port = self.param.port
-        self.prompt=  "clf> "
+
         #self.prompt = self.bright_black(f'CLF:{self.param.mode} > ')
 
             # Move text styles here
@@ -65,10 +64,13 @@ class CLF_app(cmd2.Cmd):
         )
 
     
+    
+    #rpc = RPCDevice('/dev/ttyr00')  # Modifica la porta se necessario
+    laser = Centurion("/dev/ttyr01")
+    
     ########add functions##############
 
     ##### RPC Functions ############
-    plugs=[]
     @cmd2.with_category("Instruments command")
     def do_rpc_init(self, args: argparse.Namespace) -> None:
         for oname, oparams in cfg.outlets.items():
@@ -81,36 +83,28 @@ class CLF_app(cmd2.Cmd):
                 stopbits=port_params['stopbits'],
                 timeout=port_params['timeout']
             )
-            self.plugs.append(oname)
-        
+        #dc.get_outlet("RAMAN_inst").on()
+
 
     rpc_parser = argparse.ArgumentParser()
-    rpc_parser.add_argument('value', type=str, help='name of plug', choices=plugs)
-    rpc_parser.add_argument('status', type=str, help='status of the plug on off', choices=['on','off'])
+    rpc_parser.add_argument('value', type=int, help='number of plug 1-6 (RPC_RAMAN = 1 RPC_RAD = 2 RPC_LSR = 3 RPC_RCOVER = 4 RPC_VCOVER = 5 RPC_VXM = 6)')
+    rpc_parser.add_argument('status', type=int, help='status of the plug 1=ON 0=OFF')
 
     @cmd2.with_argparser(rpc_parser)
     @cmd2.with_category("Instruments command")
     def do_rpc(self, args: argparse.Namespace) -> None:
-        
-        if args.status == 'on':
-            print('on to plug ' + args.value)
-            ind=self.plugs.index(args.value)
-            dc.get_outlet(str(self.plugs[ind])).on() 
-            
+        plugs=['RAMAN_inst', 'radiometer', 'laser', 'RAMAN_cover', 'Vert_cover', 'VXM']
+        if args.status == 1:
+            print('on to plug ' + str(args.value))
+            dc.get_outlet(plugs[args.value-1]).on()
+            comando ='on ' + str(args.value)
         else:
-            print('off to plug ' + args.value )
-            ind=self.plugs.index(args.value)
-            dc.get_outlet(str(self.plugs[ind])).off() 
-
+            print('off to plug ' + str(args.value) )
+            dc.get_outlet(plugs[args.value-1]).off()
+            comando ='off ' + str(args.value)
+        #self.rpc.send_command(comando)
+    
     ######### LASER FUNCTIONS ##################
-    @cmd2.with_category("laser command")
-    def do_lsr_connect(self, args: argparse.Namespace) -> None:
-        self.laser = Centurion("/dev/ttyr01")
-
-    @cmd2.with_category("laser command")
-    def do_lsr_close_conn(self, args: argparse.Namespace) -> None:
-        del self.laser 
-
     @cmd2.with_category("laser command")
     def do_lsr_init(self, args: argparse.Namespace) -> None:
         self.laser.set_mode(100, 1, 1, 1, 1, 1, dpw = 140, qsdelay = 145)
@@ -129,69 +123,11 @@ class CLF_app(cmd2.Cmd):
     @cmd2.with_category("laser command")
     def do_lsr_checktemps(self, args: argparse.Namespace) -> None:
         self.laser.check_temps()
-    
-    @cmd2.with_category("laser command")
-    def do_lsr_warmup(self, args: argparse.Namespace) -> None:
-        self.laser.warmup()
-
-    ######### RADIOMETER FUNCTIONS ##################
-    @cmd2.with_category("radiometer command")
-    def do_rad1_connect(self, args: argparse.Namespace) -> None:
-        rad1= Radiometer3700("/dev/ttyr02")
-        rad1.info()
-        rad1.setup()
-    
-    @cmd2.with_category("radiometer command")
-    def do_rad2_connect(self, args: argparse.Namespace) -> None:
-        rad2= Radiometer3700("/dev/ttyr04")
-        rad2.info()
-        rad2.setup()
-
-    @cmd2.with_category("radiometer command")
-    def do_rad3_connect(self, args: argparse.Namespace) -> None:
-        rad3= RadiometerOphir("/dev/ttyr05")
-        rad3.info()
-        rad3.set("$DU", 1)
 
 
-    ######### MOTOR FUNCTIONS ##################
-    motors=[]
-    @cmd2.with_category("VXM commands")
-    def do_VXM_init(self, args: argparse.Namespace) -> None:
-        for mname, mparams in cfg.motors.items():
-            port_params = cfg.get_port_params(mparams['port'])
-            dc.add_motor(mparams['id'], mname,
-                port=port_params['port'],
-                speed=port_params['speed'],
-                bytesize=port_params['bytesize'],
-                parity=port_params['parity'],
-                stopbits=port_params['stopbits'],
-                timeout=port_params['timeout']
-            )
-            self.motors.append(mname)
-        print(self.motors)
+        
 
-    @cmd2.with_category("VXM commands")
-    def do_VXM_config(self, args: argparse.Namespace) -> None:
-        dc.get_motor("UpNorthSouth").set_model(1)
-        dc.get_motor("UpNorthSouth").set_acc(2000)
-        dc.get_motor("UpEastWest").set_model(1)
-        dc.get_motor("UpEastWest").set_acc(2000)
-        dc.get_motor("LwNorthSouth").set_model(1)
-        dc.get_motor("LwNorthSouth").set_acc(2000)
-        dc.get_motor("LwPolarizer").set_model(1)
-        dc.get_motor("LwPolarizer").set_acc(2000)
-
-    @cmd2.with_category("VXM commands")
-    def do_VXM_ECAL_RAD3(self, args: argparse.Namespace) -> None:
-        dc.get_motor("UpNorthSouth").move_ABS(33250)
-        dc.get_motor("UpEastWest").move_ABS(4470)
-    
-    @cmd2.with_category("VXM commands")
-    def do_VXM_home_UP(self, args: argparse.Namespace) -> None:
-        dc.get_motor("UpNorthSouth").move_ABS0()
-        dc.get_motor("UpEastWest").move_ABS0()
-
+        
 if __name__ == '__main__':
    parser = argparse.ArgumentParser()
    parser.add_argument('--mode', default='rtu', const='rtu', nargs='?', choices=['rtu', 'tcp'], help='set modbus interface (default: %(default)s)')
