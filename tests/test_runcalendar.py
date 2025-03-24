a6dcdcb4ae0a4ba4b399238c291a1a4eced4d5de
Gennaro Tortone <gennaro.tortone@na.infn.it>
2025-03-24 10:32:31 +0000

    fix typo in test_runcalendar.py

--- tests/test_runcalendar.py
+++ tests/test_runcalendar.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from lib.RunCalendar import RunCalendar
 
-rc = RunCalendar('docs/ALLFDCalender.txt')
+rc = RunCalendar('docs/ALLFDCalendar.txt')
 for day in rc.get_next_entries(1):
     for run in rc.get_timetable_for_entry(day):
         print(run)
