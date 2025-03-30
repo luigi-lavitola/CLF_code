603a2fc5e03999e9ab7f6fa9b84caeb4405441da
Gennaro Tortone <gennaro.tortone@na.infn.it>
2025-03-30 10:03:04 +0000

    simplify test_runcalendar.py

--- tests/test_runcalendar.py
+++ tests/test_runcalendar.py
@@ -2,22 +2,14 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from lib.RunCalendar import RunCalendar
-
-from apscheduler.schedulers.background import BackgroundScheduler
 import time
-
-scheduler = BackgroundScheduler()
 
 runlist = []
 rc = RunCalendar('docs/ALLFDCalendar.txt')
 #for day in rc.get_next_entries(1):
 for day in rc.get_next_entries():
     for run in rc.get_timetable_for_entry(day):
-        scheduler.add_job(print, 'date', run_date=run.start_time)
         runlist.append(run)
-
-#scheduler.print_jobs()
-scheduler.start()
 
 sortlist = sorted(runlist, key=lambda x: x.start_time)
 
