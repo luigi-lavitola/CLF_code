7d351e1627fb42d89a0300afc1a4f29aa2494ae5
Luigi <luigi@Lenovo-Luigi.localdomain>
2025-07-21 17:56:34 +0200

    filled calibration run for ecal

--- lib/Run.py
+++ lib/Run.py
@@ -658,19 +658,197 @@ class RunCalib(RunBase):
 
     def __init__(self, dc : DeviceCollection):
         super().__init__(dc) 
-
+        self.nshots = 15
+        
     def prepare(self):
+        
         self.log(logging.INFO, "prepare")
+        self.log(logging.INFO, "configure FPGA registers for CALIBRATION run")
+        self.dc.fpga.write_register('pps_delay', 0)    #0
+        self.dc.fpga.write_bit('laser_en', 1)
+        self.dc.fpga.write_bit('timestamp_en', 1)
+
+        self.dc.fpga.write_register('pulse_width', 10_000)  # 100 us
+        self.dc.fpga.write_register('pulse_energy', 17_400) # 140 us = 174 us, maximum
+        self.dc.fpga.write_register('pulse_period', 100_000_000)  # 1000 ms 1 hz
+        self.dc.fpga.write_register('shots_num', self.nshots)
+
+        self.dc.fpga.write_register('mux_bnc_0', 0b0010)
+        self.dc.fpga.write_register('mux_bnc_1', 0b0010)
+        self.dc.fpga.write_register('mux_bnc_2', 0b0010)
+        self.dc.fpga.write_register('mux_bnc_3', 0b0010)
+        self.dc.fpga.write_register('mux_bnc_4', 0b0010)
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "turn on inverter")
+        if self.dc.fpga.read_dio('inverter') == True:
+            self.log(logging.INFO, "already on - skip")
+        else:
+            self.dc.fpga.write_dio('inverter', True)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "wait RPC and MOXA power up")
+        for _ in range(10):
+            time.sleep(1)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "turn on Radiometer outlet")
+        if self.dc.get_outlet('radiometer').status() == True:
+            self.log(logging.INFO, "already on - skip")
+        else:
+            WAIT_UNTIL_TRUE(self.dc.get_outlet('radiometer').on)
+        self.log(logging.INFO, "done") 
+
+        self.log(logging.INFO, "turn on Laser outlet")
+        if self.dc.get_outlet('laser').status() == True:
+            self.log(logging.INFO, "already on - skip")
+        else:
+            WAIT_UNTIL_TRUE(self.dc.get_outlet('laser').on)
+        self.log(logging.INFO, "done") 
+
+        self.log(logging.INFO, "turn on VXM outlet")
+        if self.dc.get_outlet('VXM').status() == True:
+            self.log(logging.INFO, "already on - skip")
+        else:
+            WAIT_UNTIL_TRUE(self.dc.get_outlet('VXM').on)
+        self.log(logging.INFO, "done") 
+
+        self.log(logging.INFO, "wait power up")
+        for _ in range(10):
+            time.sleep(1)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "radiometer 1 3700 setup")
+        self.dc.get_radiometer('Rad1').setup()
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "radiometer 2 3700 setup")
+        self.dc.get_radiometer('Rad2').setup()
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "radiometer 3 3700 setup")
+        self.dc.get_radiometer('Rad3').setup()
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "laser setup")
+        self.dc.laser.set_mode(qson = 1, dpw = 140)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "laser warmup and wait for laser fire auth")
+        self.dc.laser.warmup()
+        laser_timeout_s = 120
+        t = 0
+        while not self.dc.laser.fire_auth():
+            if t >= laser_timeout_s:
+                self.log(logging.ERROR, f"laser fire authorization timeout ({laser_timeout_s}s) - run interrupted")
+                return -1
+            self.log(logging.INFO, self.dc.laser.temperature())
+            #self.dc.laser.standby()
+            time.sleep(1)
+            t += 1
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "select vertical beam")
+        self.dc.fpga.write_dio('flipper_raman', False)
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "Motor initialization...")
+        self.dc.get_motor("UpNorthSouth").set_model(1)
+        self.dc.get_motor("UpNorthSouth").set_acc(2000)
+        self.dc.get_motor("UpEastWest").set_model(1)
+        self.dc.get_motor("UpEastWest").set_acc(2000)
+        
+        self.dc.get_motor("LwNorthSouth").set_model(1)
+        self.dc.get_motor("LwNorthSouth").set_acc(2000)
+        self.dc.get_motor("LwPolarizer").set_model(1)
+        self.dc.get_motor("LwPolarizer").set_acc(2000)
+        
         return 0
 
     def run(self):
         self.log(logging.INFO, "run")
-        time.sleep(10)
- 
+        
+        self.log(logging.INFO, "Motor moving in position for RAD3 measurement...")
+        self.dc.get_motor("UpNorthSouth").move_ABS(33250)
+        self.dc.get_motor("UpEastWest").move_ABS(4470)
+
+
+        self.log(logging.INFO, "set laser in fire mode...")
+        self.dc.laser.fire()
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "Start ECAL at 140 us")
+        
+        self.dc.fpga.write_dio('laser_en', 1)
+        self.dc.fpga.write_dio('laser_start', 1)
+        
+        for i in range(self.nshots):
+            power=self.dc.get_radiometer('Rad3').read_power()
+            seconds, counter, pps, counter_cycles = self.dc.data.read_event()
+            self.log(logging.INFO, f'power {i} shot: {power}, seconds: {seconds}, counter: {counter}, pps distance: {pps}ns, counter cycle: {counter_cycles}')
+        
+        self.dc.laser.standby()
+        
+        self.log(logging.INFO, "Rad 3 finished, going home")
+        self.dc.get_motor("UpNorthSouth").move_ABS0()
+        self.dc.get_motor("UpEastWest").move_ABS0()
+        
+        self.log(logging.INFO, "100 us energy setting now")
+        self.dc.fpga.write_register('pulse_energy', 16_400) # 140 us = 174 us, maximum
+
+        self.log(logging.INFO, "RAD2 positioning on going...")
+        self.dc.get_motor("UpNorthSouth").move_ABS(1300)
+        self.dc.get_motor("UpEastWest").move_ABS(20200)
+        
+        self.log(logging.INFO, "set laser in fire mode...")
+        self.dc.laser.fire()
+        self.log(logging.INFO, "done")
+        
+        self.log(logging.INFO, "Start ECAL at 100 us")
+        
+        self.dc.fpga.write_dio('laser_en', 1)
+        self.dc.fpga.write_dio('laser_start', 1)
+        
+        for i in range(self.nshots):
+            power=self.dc.get_radiometer('Rad2').read_power()
+            seconds, counter, pps, counter_cycles = self.dc.data.read_event()
+            self.log(logging.INFO, f'power {i} shot: {power}, seconds: {seconds}, counter: {counter}, pps distance: {pps}ns, counter cycle: {counter_cycles}')
+        
+        self.log(logging.INFO, "set laser standby")
+        self.dc.laser.standby()
+        
+        self.log(logging.INFO, "Rad 2 finished, going home")
+        self.dc.get_motor("UpNorthSouth").move_ABS0()
+        self.dc.get_motor("UpEastWest").move_ABS0()
+        
+        
     def finish(self):
         self.log(logging.INFO, "finish")
+        
+        self.log(logging.INFO, "turn off Radiometer outlet")
+        WAIT_UNTIL_TRUE(self.dc.get_outlet('radiometer').off)
+        self.log(logging.INFO, "done")
 
+        self.log(logging.INFO, "turn off Laser outlet")
+        WAIT_UNTIL_TRUE(self.dc.get_outlet('laser').off)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "turn off VXM outlet")
+        WAIT_UNTIL_TRUE(self.dc.get_outlet('VXM').off)
+        self.log(logging.INFO, "done")
+
+        self.log(logging.INFO, "turn off inverter")
+        self.dc.fpga.write_dio('inverter', False)
+        self.log(logging.INFO, "done")
+        
     def abort(self):
         self.log(logging.INFO, "abort")
+
+        self.dc.fpga.write_dio('laser_en', 0)
+
+        self.log(logging.INFO, "set laser standby")
+        self.dc.laser.standby()
+        self.log(logging.INFO, "done")
+        
         self.finish()
 
